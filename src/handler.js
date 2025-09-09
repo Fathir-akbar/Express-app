3f4decab932856a538a446097564001ff5a6f265
fathir-akbar <gamingfr655@gmail.com>
2025-09-09 10:43:53 +0700

    pembaruan buku

--- src/handler.js
+++ src/handler.js
@@ -24,3 +24,53 @@ export const addBookHandler = (req, res) => {
     },
   });
 };
+
+// menambahkan buku berdasarkan id
+
+export const getBookByIdHandler = (req, res) => {
+  const { bookId } = req.params;
+
+  const numericBookId = Number(bookId);
+
+  const book = books.find((b) => b.id === numericBookId);
+
+  if (!book) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Buku tidak ditemukan",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      book,
+    },
+  });
+};
+
+// mengubah data buku berdasarkan id
+export const updateBookByIdHandler = (req, res) => {
+  const { bookId } = req.params;
+  const { name, author } = req.body;
+  const numericBookId = Number(bookId);
+
+  const book = books.find((b) => b.id === numericBookId);
+  if (!book) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Buku tidak ditemukan",
+    });
+  }
+
+  book.name = name;
+  book.author = author;
+
+  res.status(200).json({
+    status: "success",
+    message: "Buku berhasil diubah",
+    data: {
+      book,
+    },
+  });
+};
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { getAllBooksHandler } from "./handler.js";
 import { addBookHandler } from "./handler.js";
+import { getBookByIdHandler } from "./handler.js";
+import { updateBookByIdHandler } from "./handler.js";
 
 const router = express.Router();
 
 //Routing
 router.get("/books", getAllBooksHandler);
 router.post("/books", addBookHandler);
+router.get("/books/:bookId", getBookByIdHandler);
+router.put("/books/:bookId", updateBookByIdHandler);
 
 router.get("/", (req, res) => {
   res.send("Hello World");
